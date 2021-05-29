b38e741cc2a83f5f0d897818f3ecf9d72b9b86a5
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2021-05-29 19:16:10 +0200

    completed project

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+//   GET THE ice_cream_form AND SAVE IT AS A VARIABLE
+let ice_cream_form = document.querySelector(".ice-cream-form");
+
+//   ADD A SUBMIT EVENT LISTENER TO THE ice_cream_form
+ice_cream_form.addEventListener("submit", function(e) {
+     //   PREVENT THE DEFAULT ACTION OF THE SUBMIT, WHICH IS TH EPAGE RELOAD
+     e.preventDefault();
+     //   RUN THE submit_form FUNCTION
+     submit_form();
+});
+
+//   FUNCTION TO SUBMIT THE FORM
+function submit_form() {
+     //   GET THE VALUES OF TH ETWO INPUTS
+     let ice_cream_amount = document.querySelector(".amount").value;
+     let ice_cream_cost = document.querySelector(".cost").value;
+
+     //   CHECK IF BOTH INPUTS ARE NOT EMPTY
+     if (ice_cream_amount != "" && ice_cream_cost != "") {
+          //   CHANGE THE DATA TYPES TO THE CORRECT ONES
+          ice_cream_cost = parseFloat(ice_cream_cost);
+          ice_cream_amount = parseInt(ice_cream_amount);
+
+          alert(`${ice_cream_amount} ice cream cone(s) cost R${parseFloat(ice_cream_amount * ice_cream_cost)}`);
+     } else {
+          alert("Inputs are empty")
+     }
+}
